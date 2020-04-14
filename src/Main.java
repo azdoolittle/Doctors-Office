5cc4cabb06dcefad6b76623fbe22fa3e3410d5ba
azdoolittle <drewdoolittle@gmail.com>
2020-04-14 11:49:22 -0400

    Fixed errors in Main class

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String patientName = "Stacey Manson";
+        boolean hasInsurance = false;
+        int temp = 102;
+        double numVisits = 5.02;
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient has insurance: " + hasInsurance);
+        System.out.println("Body temperature: " + temp);
+    }
+}
